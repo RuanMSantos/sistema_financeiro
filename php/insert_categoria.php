6cd8acf93c3e25ae70aa78d7abc3f539ebb72c2d
RuanMSantos <ruancauancauan@gmail.com>
2024-10-01 21:31:24 -0300

    Adiciona insert de categoria

---
+++ php/insert_categoria.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'conexao.php';
+
+$nome = $_POST['categoria'];
+$tipo = $_POST['flexRadioDefault'];
+$descricao = $_POST['descricao'];
+
+$sql = "INSERT INTO tb_categoria VALUES (null, '$nome', $tipo, '$descricao')";
+
+if ($conexao->query($sql)){
+    echo "<script>alert('Enviado com sucesso'); history.back();</script>";
+} else {
+    echo "Falha na conexão com o banco de dados";
+}
+
+?>
